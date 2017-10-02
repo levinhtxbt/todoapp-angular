2a1ea1999a13fbdd0a4e3317eeb7112b5c3f4ec7
virus <levinhtxbt@gmail.com>
2017-10-02 13:16:47 +0700

    init project

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [
@@ -14,3 +14,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
